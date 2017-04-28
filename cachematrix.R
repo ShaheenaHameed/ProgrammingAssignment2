3b830829a035a4ff64fbbc8e9482c1916a6c99fa
ShaheenaHameed <hameed.shaheena.1@gmail.com>
2017-04-28 14:01:38 +0500

    Add files via upload
    This contains all my coding saved in the same file cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,86 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Example: Caching the Mean of a Vectorless 
+## In this example we introduce the <<- operator which can be used to assign a value to an object in an environment that is different from the current environment. Below are two functions that are used to create a special object that stores a numeric vector and cache's its mean.
+
+##The first function, makeVector creates a special "vector", which is really a list containing a function to: set the value of the vector, get the value of the vector, set the value of the mean and, get the value of the mean
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
+#The following function calculates the mean of the special ???vector??? created with the above function. However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
+# Moving on to the assignment functions
+##Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
 
+## Write the following functions:
+        # 1. makeCacheMatrix: This function creates a special ???matrix??? object that can cache its inverse.
+        # 2 cacheSolve: This function computes the inverse of the special ???matrix??? returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve should retrieve the inverse from the cache.
+        # 3. Computing the inverse of a square matrix can be done with the solve function in R. For example, if X is a square invertible matrix, then solve(X) returns its inverse.
 
-## Write a short comment describing this function
+### For this assignment, assume that the matrix supplied is always invertible.
 
+# cachematrix.R:
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
+## Creating the inverse of the above matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
 }
+# Testing my functions
+test_matrix <- makeCacheMatrix(matrix(1:6, 2, 2))
+test_matrix$get()
+test_matrix$getInverse()
+cacheSolve(test_matrix)
+cacheSolve(test_matrix)
+test_matrix$getInverse()
+test_matrix$set(matrix(c(5, 5, 4, 3), 2, 2))
+test_matrix$getInverse()
+cacheSolve(test_matrix)
+cacheSolve(test_matrix)
+test_matrix$getInverse()
